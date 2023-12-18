9486ff6655b806e38fc30b878efacafb57343285
LekoArts <lekoarts@gmail.com>
2023-12-18 17:03:35 +0100

    convert SeccoCLI helper to builder pattern

--- integration/helpers/invoke-cli.ts
+++ integration/helpers/invoke-cli.ts
@@ -11,9 +11,58 @@ const fixturesLocation = join(__dirname, '..', 'fixtures')
 type CreateLogsMatcherReturn = ReturnType<typeof createLogsMatcher>
 type InvokeResult = [exitCode: number, logsMatcher: CreateLogsMatcherReturn]
 
+export function SeccoCLI() {
+  let env: Record<string, string> = {}
+  let cwd = ''
+
+  const self = {
+    setEnv: (_env: Record<string, string>) => {
+      env = _env
+      return self
+    },
+    setCwd: (_cwd: string) => {
+      cwd = _cwd
+      return self
+    },
+    invoke: (args: Array<string>): InvokeResult => {
+      const NODE_ENV = 'production'
+
+      try {
+        const results = execaSync(
+          process.execPath,
+          [builtCliLocation].concat(args),
+          {
+            cwd: join(fixturesLocation, cwd),
+            env: { NODE_ENV, ...env },
+          },
+        )
+        return [
+          results.exitCode,
+          createLogsMatcher(strip(results.stderr.toString() + results.stdout.toString())),
+        ]
+      }
+      catch (e) {
+        const execaError = e as ExecaSyncError
+        return [
+          execaError.exitCode,
+          createLogsMatcher(strip(execaError.stdout?.toString() || ``)),
+        ]
+      }
+    },
+  }
+
+  return self
+}
+
+/*
 export const SeccoCLI = {
+  // Set environment variables
+  setEnv(env: Record<string, string>): void {
+    customEnv = env
+  },
   from(relativeCwd: string) {
     return {
+      // Run the CLI with the given arguments
       invoke(args: Array<string>): InvokeResult {
         const NODE_ENV = 'production'
 
@@ -23,7 +72,7 @@ export const SeccoCLI = {
             [builtCliLocation].concat(args),
             {
               cwd: join(fixturesLocation, relativeCwd),
-              env: { NODE_ENV },
+              env: { NODE_ENV, ...customEnv },
             },
           )
           return [
@@ -42,3 +91,4 @@ export const SeccoCLI = {
     }
   },
 }
+*/
