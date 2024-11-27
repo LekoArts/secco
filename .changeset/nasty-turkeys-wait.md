c2a34454c93a38c01f3de504282f98a76c41eb62
Lennart <lekoarts@gmail.com>
2024-11-27 15:52:54 +0100

    fix: Do not double publish in pnpm workspaces with force-verdaccio (#126)

---
+++ .changeset/nasty-turkeys-wait.md
@@ -0,0 +1,5 @@
+---
+"secco": patch
+---
+
+Do not try to publish twice if workspace/catalog protocol (pnpm) is used in source with --force-verdaccio
--- src/utils/check-deps-changes.ts
+++ src/utils/check-deps-changes.ts
@@ -134,6 +134,13 @@ export async function checkDepsChanges(args: CheckDependencyChangesArgs) {
         continue
       }
 
+      if (sourcePkgJson.dependencies[key] === 'workspace:*' || sourcePkgJson.dependencies[key]?.startsWith('catalog:')) {
+        // If the source is using pnpm workspaces with workspace or catalog protocols, the dependencies were adjusted in the "adjustPackageJson" function. The change from specific versions back to "workspace:*" or "catalog:" should be ignored here
+
+        // The check is also necessary because otherwise after a "--force-verdaccio" it tries to publish the packages again
+        continue
+      }
+
       if (
         nodeModulePkgJson.dependencies[key]
         && sourcePkgJson.dependencies[key]
