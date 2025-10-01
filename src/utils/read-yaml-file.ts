f3c7dfa3eb85c3a575b887f925a07cbc4d898513
LekoArts <lekoarts@gmail.com>
2025-10-01 18:24:05 +0200

    use yaml pkg

--- src/utils/read-yaml-file.ts
+++ src/utils/read-yaml-file.ts
@@ -1,9 +1,22 @@
 import fs from 'fs-extra'
-import yaml from 'js-yaml'
-import stripBom from 'strip-bom'
+import YAML from 'yaml'
+
+function stripBom(string: string) {
+  if (typeof string !== 'string') {
+    throw new TypeError(`Expected a string, got ${typeof string}`)
+  }
+
+  // Catches EFBBBF (UTF-8 BOM) because the buffer-to-string
+  // conversion translates it to FEFF (UTF-16 BOM).
+  if (string.charCodeAt(0) === 0xFEFF) {
+    return string.slice(1)
+  }
+
+  return string
+}
 
 export function readYamlFile<T = unknown>(filepath: string) {
   const data = fs.readFileSync(filepath, 'utf8')
 
-  return yaml.load(stripBom(data)) as T
+  return YAML.parse(stripBom(data)) as T
 }
